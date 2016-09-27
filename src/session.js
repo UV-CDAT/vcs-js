9f9618a80f197d55fe9720303f119f88618d7449
Dan Lipsa <dan.lipsa@kitware.com>
2016-09-27 16:52:46 -0400

    Printing variables works using cdms2 and all plots work.

--- src/session.js
+++ src/session.js
@@ -3,6 +3,8 @@ import { createClient } from 'ParaViewWeb/IO/WebSocket/ParaViewWebClient';
 
 import Promise from './promise';
 import createFile from './file';
+import plotly from './plotly';
+import vtkweb from './vtkweb';
 
 export default (url, username, password) => {
   // vtkweb launcher uses a "secret" key rather than user/pass
@@ -21,13 +23,16 @@ export default (url, username, password) => {
 
   // create a pvw client object after the session is ready
   const pvw = connectionPromise.then((connection) => {
-    return createClient(connection, ['MouseHandler', 'ViewPort', 'ViewPortImageDelivery', 'FileListing']);
+    return createClient(
+       connection,
+      ['MouseHandler', 'ViewPort', 'ViewPortImageDelivery', 'FileListing']);
   });
 
   const session = {
     close() {
       return connectionPromise.then((connection) => connection.destroy());
     },
+
     files(path) {
       return pvw
         .then((client) => client.FileListing.listServerDirectory(path))
@@ -35,8 +40,55 @@ export default (url, username, password) => {
           return filesObject.files.map((file) => createFile(session, file.label));
         });
     },
+
+    getgraphicsmethod(methodtype, methodname) {
+      return Promise.delay(0, {
+        type: methodtype,
+        name: methodname,
+        session: this,
+      });
+    },
+
     client() {
       return pvw;
+    },
+
+    /**
+     * Creates a new canvas.
+     */
+    init(el) {
+      const outerSession = this;
+
+      const canvas = {
+        el,
+        windowId: 0,
+        session: outerSession,
+
+        plot(dataSpec, template,
+             graphicsMethodType, graphicsMethodName, renderingType) {
+          switch (renderingType) {
+            case 'client': {
+              return plotly(this, dataSpec, template,
+                            graphicsMethodType, graphicsMethodName);
+            }
+            case 'server':
+              return vtkweb(this, dataSpec, template,
+                            graphicsMethodType, graphicsMethodName);
+
+            default:
+              return Promise.reject(new Error('Invalid renderingType'));
+          }
+        },
+
+        close() {
+          if (this.windowId) {
+            console.log(`close canvas ${this.windowId}`);
+            this.session.client().then(
+              (client) => client.session.call('cdat.view.close', [this.windowId]));
+          }
+        },
+      };
+      return canvas;
     },
   };
 
